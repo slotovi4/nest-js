20965d06d70514a33361fcfbaba5f450c1681e8d
slotovi4 <roman.bruns@mail.ru>
2021-08-04 19:14:04 +0300

    feat: add TypegooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,20 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 
+import { getMongoConfig } from './configs/mongo.config';
+
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
 		AuthModule,
 		TopPageModule,
 		ProductModule,
