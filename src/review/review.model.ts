d767931c51586e417c3d96d757dbc6faae5352d5
slotovi4 <roman.bruns@mail.ru>
2021-08-05 17:00:34 +0300

    feat: add TypegooseModule to review

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,24 @@
-export class ReviewModel {
-	_id: string;
-	_name: string;
-	_title: string;
-	_description: string;
-	_rating: number;
-	_createdAt: Date;
+import { prop } from '@typegoose/typegoose';
+
+import type { Base } from '@typegoose/typegoose/lib/defaultClasses';
+
+export class ReviewModel implements Base {
+
+	@prop()
+	name: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	description: string;
+
+	@prop()
+	rating: number;
+
+	@prop()
+	createdAt: Date;
+
+	_id: Base['_id'];
+	id: string;
 }
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,8 +1,18 @@
 import { ReviewController } from './review.controller';
 
+import { ReviewModel } from './review.model';
+
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-	controllers: [ReviewController]
+	controllers: [ReviewController],
+	imports: [TypegooseModule.forFeature([{
+		typegooseClass: ReviewModel,
+		schemaOptions: {
+			collection: 'Review',
+			timestamps: true,
+		}
+	}])]
 })
 export class ReviewModule { }
